0da7cad2f23d4664cff07e809e883a0acbdc7376
HugoMillet1 <116892953+HugoMillet1@users.noreply.github.com>
2023-08-20 18:14:50 +0200

    Add files via upload

---
+++ MarmosetBG.py
@@ -0,0 +1,29 @@
+from GenericBG import Network
+from netpyne import sim
+
+class MarmosetBG( Network ):
+    def __init__( self ):
+        super( MarmosetBG, self ).__init__()
+        self.set_marmoset()
+
+
+    def set_marmoset( self ):
+        genotype = [ 0.47148627, 0.         , 1.        , 0.24748603, 1.        , 0.0930143,
+                     1.        ,  0.01593558, 0.9980333 , 0.19937477, 1.        , 0.48306456,
+                     0.48590738,  0.14889275 ]
+        self.set_genotype( genotype )
+
+
+if __name__=='__main__':
+    network = MarmosetBG()
+    network.simulate()
+    spikes_dict = network.extractSpikes()
+    network.plotElaborate()
+    #print('SPIKES')
+    #for key in spikes_dict.keys():
+       # print(key)
+       # print( [s.times for s in spikes_dict[key]] )
+    #mfr = network.extractMFR()
+    #print(mfr)
+    print("End")
+    #print('LFP DATA', sim.allSimData['LFPCells'])
